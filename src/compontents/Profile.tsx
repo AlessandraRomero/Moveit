3ce5f51e5a39e7602397f0891feab149f947b40d
AlessandraRomero <alessandraromeiro098@gmail.com>
2021-03-01 19:37:45 -0400

    Importando e chamando a função completeChallenge

--- src/compontents/Profile.tsx
+++ src/compontents/Profile.tsx
@@ -1,5 +1,12 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/Profile.module.css'
+
+
+
 export function Profile() {
+const { level } = useContext(ChallengesContext)
+
     return (
         <div className={styles.profileContainer}>
             <img src="https://github.com/alessandraromero.png" alt="Alessandra Romero" />
@@ -7,7 +14,7 @@ export function Profile() {
                 <strong>Alessandra Romero</strong>
                 <p>
                     <img src="icons/level.svg" alt="" />
-                   Level 1
+                   Level { level }
              </p>
             </div>
         </div>
