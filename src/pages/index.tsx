4b7d86678b1e4caf09e8d8b205914df73b17969b
AlessandraRomero <alessandraromeiro098@gmail.com>
2021-03-09 09:54:01 -0400

    Salvando informações através dos cookies

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,9 +7,23 @@ import { ChallengeBox } from '../components/ChallengeBox'
 
 import Head from 'next/head'
 import { CountdownProvider } from '../contexts/CountdownContext'
+import { GetServerSideProps } from 'next'
+import { ChallengesProvider } from '../contexts/ChallengesContext'
+import { prependOnceListener } from 'process'
 
-export default function Home() {
+interface homeProps{
+  level: number;
+  currentExperience: number;
+  challengesCompleted: number;
+}
+
+export default function Home(props) {
   return (
+    <ChallengesProvider 
+    level={props.level} 
+    currentExperience={props.currentExperience}
+    challengesCompleted={props.challengesCompleted}
+    >
     <div className={styles.container}>
       <Head>
         <title>Início | Move.it</title>
@@ -28,5 +42,19 @@ export default function Home() {
       </section>
       </CountdownProvider>
     </div>
+    </ChallengesProvider>
   );
+}
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+
+  const { level, currentExperience, challengesCompleted } = ctx.req.cookies
+
+  return{
+    props:{
+      level: Number(level),
+      currentExperience: Number(currentExperience),
+      challengesCompleted: Number(challengesCompleted),
+    }
+  }
 }
